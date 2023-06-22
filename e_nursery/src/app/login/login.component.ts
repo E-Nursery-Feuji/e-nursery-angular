bdb8eeed4a1cdf86c4f14a69b5fb4332f7d688ea
Priya Patel <62372226+priya1523@users.noreply.github.com>
2023-06-22 22:17:30 +0530

    login-1 (#3)
    created login page,apply validation,apply css

--- e_nursery/src/app/login/login.component.ts
+++ e_nursery/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm:FormGroup;
+  submitted:boolean = false;
 
-  ngOnInit(): void {
+  constructor(private formBuilder:FormBuilder)
+  {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
   }
 
+  ngOnInit(): void
+  {
+   
+  }
+
+  loginSubmit()
+  {
+    this.submitted = true;
+    if (this.loginForm.invalid) 
+    {
+      return;
+    }
+  }
 }
