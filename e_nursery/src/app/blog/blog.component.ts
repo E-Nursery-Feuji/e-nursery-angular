c1380e2fcfc98969a522d8e69ea9b1725aa0fdba
HarithaPedapudi <129718846+HarithaPedapudi@users.noreply.github.com>
2023-07-06 09:20:03 +0530

    displayedBlogs (#33)

--- e_nursery/src/app/blog/blog.component.ts
+++ e_nursery/src/app/blog/blog.component.ts
@@ -14,21 +14,27 @@ export class BlogComponent implements OnInit {
 
   show: boolean = true; // Variable to control form visibility
   blog:Blog=new Blog()
-  blogs?: Blog[];
+  blogs: Blog[]=[];
+  images:Image[]=[];
+  file?:any
+  picture:any
+  openedBlogId: number | null = null;
   constructor(private blogService: BlogService){}
 
   saveBlog() {
     console.log(this.blog);
-    let blog = {
+    this. blog = {
+      "id":this.blog.id,
       "title": this.blog.title,
       "uploaded_by": this.blog.uploaded_by,
       "description": this.blog.description,
-      "status": "active",
-      "image": { "id": 0, "image": this.blog.image }
+      "status": "InActive",
+      //"image": { "id": 0, "image": this.blog.image }
     };
+    // this.file=this.blog.image
 
-    console.log("locAL BLOG "+blog);
-
+    console.log(this.file);
+ this.blogService.saveBlogImage(this.blog,this.file)
 
     // this.blogService.saveBlog(blog).subscribe(savedBlog => {
     //   console.log('Blog saved:', savedBlog);
@@ -36,36 +42,91 @@ export class BlogComponent implements OnInit {
     //   let message = savedBlog
     //   Swal.fire({
     //     title: 'Confirmation',
-    //     text: 'message',
+    //     text: message,
     //     icon: 'success'
     //   })
     //   this.getBlogs();
     // });
 
   }
+  onFileChange(event:any){
+    this.file=event.target.files[0]
+
+  }
 
   ngOnInit() {
-    this.getBlogs();
+     this.getAllBlogs();
+     this.getImages();
   }
-
-  getBlogs() {
-    this.blogService.getAllBlogs().subscribe(data => {
-      console.log(data);
-      this.blogs = data;
-
-      // Iterate over each blog in the array
-      this.blogs.forEach(blog => {
-        let imageLink: string = blog.image!.image; // Access the image property of each blog
-        imageLink = imageLink.substring(2, imageLink.length - 1);
-        blog.image!.image = imageLink;
-        console.log("Image link: " + imageLink);
-      });
-      console.log("from compo ", this.blogs);
-    });
+  openBlog(blog: any) {
+    this.openedBlogId = blog.id;
   }
+  closeBlog(blog: any) {
+    this.openedBlogId = null;
+  }
+  toggleBlog(blog: any) {
+    if (this.openedBlogId === blog.id) {
+      this.openedBlogId = null;
+    } else {
+      this.openedBlogId = blog.id;
+    }
+  }
+    // Function to check if a blog is currently opened
+    isBlogOpened(blog: any): boolean {
+      return this.openedBlogId === blog.id;
+    }
+
+
+    updatestatus(id:number) {
+      console.log(id)
+
+      this.blogService.updateStatus(id).subscribe((data) => {
+        console.log(data)
+        console.log('Status updated successfully.');
+        this.getAllBlogs();
+        this.getImages();
+
+      });
+    }
+
+
+
+
+  // getBlogs() {
+  //   this.blogService.getAllBlogs().subscribe(data => {
+  //     console.log(data);
+  //     this.blogs = data;
+
+  //     // Iterate over each blog in the array
+  //     this.blogs.forEach(blog => {
+  //       // let imageLink: string = blog.image!.image; // Access the image property of each blog
+  //       imageLink = imageLink.substring(2, imageLink.length - 1);
+  //       blog.image!.image = imageLink;
+  //       console.log("Image link: " + imageLink);
+  //     });
+  //     console.log("from compo ", this.blogs);
+  //   });
+  // }
 
   openDialog() {
     console.log("click");
     this.show = !this.show; // Toggle the value of showForm variable
+  }
+
+  getImages(){
+    this.images=[];
+    this.blogService.getImages().subscribe(data=>{
+this.images=this.images?.concat(data)
+console.log(data)
+    })
+  }
+
+  getAllBlogs(){
+    this.blogs=[];
+    this.blogService.getAllBlogs().subscribe(data=>{
+      this.blogs=this.blogs?.concat(data)
+      console.log(data)
+      console.log(this.blogs)
+    })
   }
 }
--- e_nursery/src/app/services/BlogService.ts
+++ e_nursery/src/app/services/BlogService.ts
@@ -3,6 +3,9 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { BlogRepository } from "../repository/BlogRepository";
 import { HttpClient } from '@angular/common/http';
+import { log } from 'console';
+import { Image } from '../model/image';
+import { Router } from '@angular/router';
 
 
 @Injectable({
@@ -13,16 +16,38 @@ export class BlogService{
   // private apiUrl = 'your-api-url';
 
 
-constructor(private blogRepository:BlogRepository,private http: HttpClient){}
+constructor(private blogRepository:BlogRepository,private http: HttpClient,private router:Router){}
 blogs?:Blog[]
 
 getAllBlogs():Observable<Blog[]>{
  return this.blogRepository.getAllBlogs()
 
   }
+  getImages():Observable<Image[]>{
+    return this.blogRepository.getImages()
 
-  saveBlog(blog: Blog) {
+     }
+
+
+  saveBlogImage(blog: Blog,image:any) {
+    const imageData= new FormData()
+    imageData.append('image',image,image.name);
     // Implement the logic to save the blog to the API
-    return this.blogRepository.saveBlog(blog)
+    return this.blogRepository.saveImage(imageData).subscribe(data=>{
+      blog.image_id=data.id
+      this.saveBlog(blog)
+      console.log("bbbbbbbbbbb")
+      console.log(blog)
+       this.router.navigateByUrl("admin");
+    })
+
   }
+  saveBlog(blog:Blog){
+    this.blogRepository.saveBlog(blog).subscribe(data=>console.log(data))
+  }
+
+  updateStatus(id:number){
+    return this.blogRepository.updateStatus(id);
+  }
 }
+
