a136a1ed75ffff24c5a46940e7fb4cd8cd11b796
sambanna07 <115611654+sambanna07@users.noreply.github.com>
2023-07-10 21:17:07 +0530

    a

--- e_nursery/src/app/plants/plants.component.ts
+++ e_nursery/src/app/plants/plants.component.ts
@@ -27,7 +27,44 @@ constructor(private productservice: Productservice,private orderService:OrderSer
 
   //add to cart
   addToCart(plant:Product){
-    // console.log(plant)
-    this.orderService.addToCart(plant);
+   this.getCart();
+   return this.orderService.addToCart(plant);
+  }
+
+  //get products which in cart
+  getCart(){
+   return this.orderService.getCart();
+  }
+
+  //for add to cart button swap
+  getAdd(id:number)
+  {
+    const index=this.getCart().findIndex(c=>c.product==id)
+    if(index == -1)
+      return false;
+    else
+      return true;
+  }
+
+  // for get the quantity
+  getQuantity(id:number)
+  {
+    return this.getCart().filter(c=>c.product==id)[0].quantity;
+  }
+
+  //for quantity add
+  addQuantity(id:number)
+  {
+    const cart=this.getCart().filter(c=>c.product==id)[0];
+    cart.quantity=Number(cart.quantity)+1;
+    this.orderService.updateCartQuantity(cart);
+  }
+
+  // for quantity remove
+  removeQuantity(id:number)
+  {
+    const cart=this.getCart().filter(c=>c.product==id)[0];
+    cart.quantity=cart.quantity-1;
+    this.orderService.updateCartQuantity(cart);
   }
 }
