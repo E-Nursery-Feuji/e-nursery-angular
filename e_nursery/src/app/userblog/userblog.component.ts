feceb333f05bf68400bad3f7345b95dc10709bc1
Suresh-130 <129723623+Suresh-130@users.noreply.github.com>
2023-07-10 12:58:02 +0530

    minor-changes (#51)

--- e_nursery/src/app/userblog/userblog.component.ts
+++ e_nursery/src/app/userblog/userblog.component.ts
@@ -15,16 +15,12 @@ export class UserblogComponent implements OnInit {
   blogShow:boolean=false;
   blogForm: FormGroup;
   submitted:boolean=false;
-  description:string="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam nihil perspiciatis repellendus officiis voluptas voluptatum perferendis molestiae nesciunt quis a, unde minus? Voluptatibus alias ea impedit nostrum sunt! Voluptatem, excepturi!"+
-  "Asperiores distinctio debitis, blanditiis vitae assumenda hic ullam rem dolor magni cum ipsa quos amet possimus, sint reprehenderit harum expedita doloremque, voluptatem neque. Quidem ipsum quibusdam debitis perferendis, sapiente aspernatur."+
-  "Consequuntur aspernatur deleniti illum odit in similique accusantium commodi at dolore ex illo error quis possimus laborum adipisci iure soluta, eveniet a ullam magni eos earum ratione eaque nam. In!"+
-  "Exercitationem aliquam culpa non, consequuntur unde labore dicta repellendus sed quod! Aperiam numquam vel blanditiis sed est. Fuga, assumenda similique, magni reprehenderit in neque fugiat vero aperiam, hic accusantium asperiores?"+
-  "Alias, incidunt, blanditiis quam hic voluptate optio, ipsum tempora consectetur perferendis ea adipisci. Sed alias nesciunt qui adipisci nam quo doloribus? Officiis voluptatibus corporis veritatis necessitatibus, error provident nam quam.";
+  blog:Blog=new Blog();
+
   showReadMore:boolean = false;
 
   //-------------------------------------
   show: boolean = true; // Variable to control form visibility
-  blog:Blog=new Blog()
   blogs: Blog[]=[];
   images:Image[]=[];
   file?:any
@@ -53,8 +49,9 @@ export class UserblogComponent implements OnInit {
   }
 
   //for open the blog modal
-  blogModal()
+  blogModal(blog:Blog)
   {
+    this.blog=blog;
     this.blogShow=!(this.blogShow);
   }
 
@@ -81,31 +78,13 @@ export class UserblogComponent implements OnInit {
     }
     if(this.blogForm.valid)
     {
-      this.blog=this.blogForm.value()
-      this.blog.status=="Inactive"
+      this.blog=this.blogForm.value
+      this.blog.status="InActive"
       this.blogService.saveBlogImage(this.blog,this.file)
 
-      //console.log(this.blog)
-      console.log("Success")
     }
   }
 
-  // -----------------------------------------------------------
-  // saveBlog() {
-  //   console.log(this.blog);
-  //   this. blog = {
-  //     "id":this.blog.id,
-  //     "title": this.blog.title,
-  //     "uploaded_by": this.blog.uploaded_by,
-  //     "description": this.blog.description,
-  //     "status": "InActive",
-  //     //"image": { "id": 0, "image": this.blog.image }
-  //   };
-  //   // this.file=this.blog.image
-
-  //   console.log(this.file);
-  //   this.blogService.saveBlogImage(this.blog,this.file)
-  // }
 
   onFileChange(event:any){
     this.file=event.target.files[0]
