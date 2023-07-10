595feb8762427f3532084585d468c24d5d0dfa54
sambanna07 <115611654+sambanna07@users.noreply.github.com>
2023-07-10 12:00:27 +0530

    small bug

--- e_nursery/src/app/services/order.service.ts
+++ e_nursery/src/app/services/order.service.ts
@@ -22,12 +22,14 @@ constructor(private orderReposistoryService:OrderReposistoryService,private cust
 addToCart(plant:Product){
   this.cart.id=0;
   this.cart.quantity=1;
-  this.cart.product_id=plant.id;
+  this.cart.product=plant.id;
   const email=this.JwttokenService.getEmailFromJwtToken()
   this.customerRepository.customerByEMail(email).subscribe(
     (response) =>{
       const customer = Object.values(response)[0];
-      this.cart.customer_id=customer.id;
+      console.log("customer id")
+      console.log(customer.id);
+      this.cart.customer=customer.id;
     },
     (error) =>{
       Swal.fire({
