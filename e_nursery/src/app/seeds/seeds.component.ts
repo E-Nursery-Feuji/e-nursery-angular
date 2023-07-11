d422599800a178b47e7b4bd0253837aa64d03a4f
sambanna07 <115611654+sambanna07@users.noreply.github.com>
2023-07-11 10:47:37 +0530

    adminsidesearch (#57)
    admin side searching css and button css of add admin. add
    product ,add  blogs

--- e_nursery/src/app/seeds/seeds.component.ts
+++ e_nursery/src/app/seeds/seeds.component.ts
@@ -1,3 +1,5 @@
+import { Product } from '../model/product';
+import { OrderService } from './../services/order.service';
 import { Productservice } from './../services/productservice.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeedsComponent implements OnInit {
 
-  constructor(private productService:Productservice) { }
+  constructor(private productService:Productservice,private orderService:OrderService) { }
 
   ngOnInit() {
   }
@@ -22,5 +24,48 @@ export class SeedsComponent implements OnInit {
   {
     return this.productService.getImages();
   }
+
+  //add to cart
+  addToCart(seed:Product){
+    this.getCart();
+    return this.orderService.addToCart(seed);
+   }
+
+   //get products which in cart
+   getCart(){
+    return this.orderService.getCart();
+   }
+
+   //for add to cart button swap
+   getAdd(id:number)
+   {
+     const index=this.getCart().findIndex(c=>c.product==id)
+     if(index == -1)
+       return false;
+     else
+       return true;
+   }
+
+   // for get the quantity
+   getQuantity(id:number)
+   {
+     return this.getCart().filter(c=>c.product==id)[0].quantity;
+   }
+
+   //for quantity add
+   addQuantity(id:number)
+   {
+     const cart=this.getCart().filter(c=>c.product==id)[0];
+     cart.quantity=Number(cart.quantity)+1;
+     this.orderService.updateCartQuantity(cart);
+   }
+
+   // for quantity remove
+   removeQuantity(id:number)
+   {
+     const cart=this.getCart().filter(c=>c.product==id)[0];
+     cart.quantity=cart.quantity-1;
+     this.orderService.updateCartQuantity(cart);
+   }
 
 }
